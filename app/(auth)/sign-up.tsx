1ed8bd256bbebe85c2151db82604b1315e803436
lakshay-aggarwal-codes <08817711624_aiml@vipstc.edu.in>
2026-08-23 11:10:00 +0530

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Link href="/(auth)/sign-in">
+                Existing Account
+            </Link>
+        </View>
+    );
+};
+
+export default SignUp;
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import { styled } from "nativewind";
+import {SafeAreaView as RNSafeAreaView} from 'react-native-safe-area-context';
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Subscriptions = () => {
+    return (
+        <SafeAreaView className="flex-1 p-5 bg-background">
+            <Text>Subscriptions</Text>
+         </SafeAreaView>
+    );
+};
+
+export default Subscriptions;
